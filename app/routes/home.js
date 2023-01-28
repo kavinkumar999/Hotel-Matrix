e6d409279283dbb3f8df8b12b020444613b8b1f7
kavinkumar999 <kavinkumarnkm007@gmail.com>
2023-01-28 22:21:57 +0530

    feat: format data in the home page

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,65 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
-    // return "data"
-    return [
-      {
-        id: 'item1',
-        name: 'Dosa',
-        img: 'c1.png',
-        price: "30",
-        catergory: '1',
-      },
-      {
-        id: 'item2',
-        name: 'Idly',
-        img: 'c2.png',
-        price: "130",
-        catergory: '2',
-      },
-      {
-        id: 'item2',
-        name: 'Idly',
-        img: 'c2.png',
-        price: "130",
-        catergory: '2',
-      },
-      {
-        id: 'item3',
-        name: 'poori',
-        img: 'c3.png',
-        price: "230",
-        catergory: '2',
-      },
-      {
-        id: 'item3',
-        name: 'poori',
-        img: 'c3.png',
-        price: "230",
-        catergory: '2',
-      },
-      {
-        id: 'item4',
-        name: 'vadai',
-        img: 'c4.png',
-        price: "100",
-        catergory: '3',
-      },
-      {
-        id: 'item4',
-        name: 'vadai',
-        img: 'c6.png',
-        price: "80",
-        catergory: '3',
-      },
-      {
-        id: 'item4',
-        name: 'vadai',
-        img: 'c7.png',
-        price: "90",
-        catergory: '3',
-      },
-    ];
+  async model() {
+    let response = await fetch('/api/food.json');
+    let parsed = await response.json();
+    return parsed;
   },
 });
