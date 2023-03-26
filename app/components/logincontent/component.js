f7a670e0b2819b7be18f5c746f415c73d8a8ca53
kavinkumar999 <kavinkumarnkm007@gmail.com>
2023-03-26 19:44:01 +0530

    feat: ember notify configuration

--- app/components/logincontent/component.js
+++ app/components/logincontent/component.js
@@ -1,9 +1,12 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  notify: service(),
 
   actions: {
     login() {
+      this.notify.success('Hello there!');
       // login validation
     },
     close() {
