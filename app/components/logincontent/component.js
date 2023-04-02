6b0b553b840a08d7ae02b1a90c98e60701be54f0
kavinkumar999 <kavinkumarnkm007@gmail.com>
2023-04-02 19:00:42 +0530

    Login validation

--- app/components/logincontent/component.js
+++ app/components/logincontent/component.js
@@ -3,11 +3,19 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   notify: service(),
+  router: service(),
+  email: '',
+  password: '',
 
   actions: {
     login() {
-      this.notify.success('Hello there!');
       // login validation
+      if (this.email === 'kavin' && this.password === 'kavin') {
+        this.notify.success('Hurray!, Login success');
+        this.router.transitionTo('home');
+      } else {
+        this.notify.warning('Invalid email or Password');
+      }
     },
     close() {
       this.attrs.close();
